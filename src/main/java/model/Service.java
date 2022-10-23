7eb5676cfaf56980aed9c485f41f8af4cf8f230f
VaidehiPatel13 <83052335+VaidehiPatel13@users.noreply.github.com>
2022-10-23 23:27:49 +1100

    Add files via upload

--- src/main/java/model/Service.java
+++ src/main/java/model/Service.java
@@ -99,10 +99,13 @@ public class Service {
      */
 
     public boolean addCustomerToService(Customer customer, Franchise fID) throws IllegalServiceException{
+//    	Get the number of service a customer is using and check if the customer does not hold more than 3 services
         if (customer.getNumberOfServices() >= 3) {
             return false;
         }
 
+//        Get the number of customers associated with the service offered by a franchise
+//        Test that the service offered by a franchise cannot have more than 500 customers
         List<Service> fID_service_list = fID.getServices();
         for (Service srv: fID_service_list){
             List<Customer> franchise_customers = srv.getCustomer();
@@ -112,22 +115,23 @@ public class Service {
              
         }
 
-
+//		Throw an exception if a customer is already using the same service
         for (Customer curr_customer:getCustomer()){
             if (customer.getId() == curr_customer.getId()){
                 throw new IllegalServiceException("Service is already being used by the same customer");
             }
         }
 
+//       Add the customer to the customer list
         List<Customer> curr_customers = getCustomer();
         curr_customers.add(customer);
         
-
+//        Increase the number of service associated with the customer by 1
         customer.setNumberOfServices(customer.getNumberOfServices() + 1);
 
 
         
-
+//        Check that there are no duplicates of the service in the list
         if (fID_service_list.contains(this) == false) {
         	fID_service_list.add(this);
         }
